28e70ea9d490f211daf118ee6c55ec7f6e7c9254
Rohan Hirekerur <rohanhirekerur@gmail.com>
2019-09-01 00:48:05 +0530

    landmarks generation added and audio visualisation added

--- pre-processing.py
+++ pre-processing.py
@@ -2,9 +2,13 @@ import cv2
 import os
 from glob import glob
 import moviepy.editor as mp
-
+import matplotlib.pyplot as plt
+from scipy.io import wavfile as wav
+from scipy.fftpack import fft
+import numpy as np
 
 input_path = "res/video"
+audio_path = "res/audio"
 video_frames_path = "res/video-frames"
 audio_frames_path = "res/audio-frames"
 
@@ -13,6 +17,9 @@ if not os.path.exists(video_frames_path):
 
 if not os.path.exists(audio_frames_path):
     os.mkdir(audio_frames_path)
+
+if not os.path.exists(audio_path):
+    os.mkdir(audio_path)
 
 types = ("*.mp4")
 video_path_list = []
@@ -23,8 +30,9 @@ for file in types:
 total = len(video_path_list)
 
 for i, input_file in enumerate(video_path_list):
-    print("Generating frames for {}".format(input_file))
-    file_name = input_file[12:-4]
+    print("Generating video frames for {}".format(input_file))
+    file_name = input_file[10:-4]
+
     if not os.path.exists(video_frames_path + "/{}".format(file_name)):
         os.mkdir(video_frames_path + "/{}".format(file_name))
     cap = cv2.VideoCapture(input_file)
@@ -37,24 +45,30 @@ for i, input_file in enumerate(video_path_list):
         # print('Read a new frame: ', success)
         count += 1
 
-    print("Extracting audio from {}".format(input_file))
+
+    print("Extracting audio clips from {}".format(input_file))
     clip = mp.VideoFileClip(input_file)
-    clip.audio.write_audiofile(audio_frames_path + "/{}.wav".format(file_name))
+    clip.audio.write_audiofile(audio_path + "/{}.wav".format(file_name))
+
+
+    print("Generating audio frames for {}".format(input_file))
+
+    if not os.path.exists(audio_frames_path + "/{}".format(file_name)):
+        os.mkdir(audio_frames_path + "/{}".format(file_name))
+
+    rate, data = wav.read(audio_path + "/{}.wav".format(file_name))
+    fft_out = fft(data)
+    ff = np.array(np.real(fft_out))
+    frame_points_array = np.array_split(ff,count)
+
+    count = 0
+    for audio_frame in frame_points_array:
+        plt.plot(audio_frame)
+        plt.savefig(audio_frames_path + "/{}/frame{}.jpg".format(file_name,count))
+        plt.clf()
+        count += 1
+
     print("Done!\n")
 
-# import matplotlib.pyplot as plt
-# from scipy.io import wavfile as wav
-# from scipy.fftpack import fft
-# import numpy as np
-# rate, data = wav.read("test.wav")
-# fft_out = fft(data)
-# print(fft_out)
-# ff = np.array(np.real(fft_out))
-# # ff = ff.astype(dtype=int)
-# print(ff)
-# # x, y = ff.T
-# # plt.scatter(x,y)
-# # print(ff,fft_out, np.abs(fft_out))
-# plt.plot(ff)
-# plt.show()
+
 
